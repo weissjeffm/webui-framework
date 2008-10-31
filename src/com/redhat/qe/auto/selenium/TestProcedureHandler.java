bad612813eb62ac201a6afd8efe580adc1775c8c
jweiss <jweiss@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2008-10-31 21:01:06 +0000

    IN PROGRESS - issue SAND-17: Logger that only logs actions (and higher), in order to upload them to Testopia http://jira.rhq-project.org/browse/SAND-17
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@178338 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

---
+++ src/com/redhat/qe/auto/selenium/TestProcedureHandler.java
@@ -0,0 +1,32 @@
+package com.redhat.qe.auto.selenium;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ * @author jweiss
+ * Class to take in-memory log statements of selenium actions,
+ * format them and return them as a big formatted string.
+ *
+ */
+public class TestProcedureHandler extends Handler {
+
+	@Override
+	public void close() throws SecurityException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void publish(LogRecord record) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
