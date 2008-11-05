31ddbdb55e15d288ef0c9d4d2b8428341c17b278
jweiss <jweiss@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2008-11-05 21:26:39 +0000

    NEW - bug 470125: testopia: create subclass for common code https://bugzilla.redhat.com/show_bug.cgi?id=470125
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@178494 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

--- src/testopia/API/TestopiaObject.java
+++ src/testopia/API/TestopiaObject.java
@@ -1,5 +1,18 @@
 package testopia.API;
 
-public class TestopiaObject {
+import java.util.Arrays;
 
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+
+public abstract class TestopiaObject {
+
+	protected XmlRpcClient client;
+
+	
+	protected int callXmlrpcMethod(String methodName, Object... params) throws XmlRpcException{	
+		return (Integer) client.execute(methodName,	Arrays.asList(params));	
+	}
+
+	
 }
