eab6e96bf02dd9c0b182c6261d5b0af59ed88d6f
Steve Salevan <Steve Salevan@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2008-11-05 21:46:43 +0000

    Adding a neat generic object list method to an abstract class that all Testopia XMLRPC API methods will gain via extending it
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@178495 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

--- src/testopia/API/TestopiaObject.java
+++ src/testopia/API/TestopiaObject.java
@@ -1,6 +1,7 @@
 package testopia.API;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
@@ -8,11 +9,36 @@ import org.apache.xmlrpc.client.XmlRpcClient;
 public abstract class TestopiaObject {
 
 	protected XmlRpcClient client;
-
+	protected String listMethod;
 	
-	protected int callXmlrpcMethod(String methodName, Object... params) throws XmlRpcException{	
-		return (Integer) client.execute(methodName,	Arrays.asList(params));	
+	protected Object callXmlrpcMethod(String methodName, Object... params) throws XmlRpcException{	
+		return (Object) client.execute(methodName, Arrays.asList(params));	
 	}
 
-	
+	/**
+	 * Generic method designed to obtain a list of objects that match parameters
+	 * supplies in provided HashMap object
+	 * @param values a HashMap with the parameters that will be searched for;
+	 * if you supply the pair {"plan_id": 5}, plan_id #5 will be returned. Any combination
+	 * of attributes can be entered and the result will be all matches that fit 
+	 * the input values
+	 * @return list of matching objects
+	 */
+	public Object[] getList(HashMap<String, Object> values)
+	{
+		//some Testopia objects have no listing mechanism
+		if(listMethod == null)
+			return null;
+		try 
+		{
+			Object[] result = (Object[]) this.callXmlrpcMethod(listMethod, values);
+			return result;
+		}			
+		
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
