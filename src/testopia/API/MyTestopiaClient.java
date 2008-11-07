8d0e5b31a8ba8ad4210e4e4ac7430e92b4f16763
Steve Salevan <Steve Salevan@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2008-11-07 16:52:01 +0000

    adding a method from rhn satellite testing framework for eventual integration
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@178606 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

--- src/testopia/API/MyTestopiaClient.java
+++ src/testopia/API/MyTestopiaClient.java
@@ -3,9 +3,11 @@ package testopia.API;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Hashtable;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpState;
+import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.XmlRpcRequest;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
@@ -36,4 +38,26 @@ public class MyTestopiaClient {
 	    
 
 	}*/
+	
+	private int getNextTestRunID(String caseID) throws XmlRpcException{
+		Object[] params = new Object[]{caseID};
+		Object[] result;
+		
+		try{
+			result = (Object[]) client.execute("Bugzilla.Testopia.Webservice.TestCase.get_case_run_history", params);
+		}
+		catch (XmlRpcException e){
+			log.info("Encountered an error while gathering testrun IDs!");
+			throw e;
+		}
+		
+		int highest = -1;
+		for (int i=0;i<result.length;i++){
+			Hashtable elem = (Hashtable) result[i];
+			int id = (Integer) elem.get("run_id");
+			if (id > highest)
+			   highest = id;
+		}
+		return (highest + 1);
+	}
 }
