c72ad89df2cb6e8d48e44dfb87b63e60e80c2607
jweiss <jweiss@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2008-12-11 19:57:35 +0000

    NEW - bug 475631: JON 2.2 version detection and initial testing of automation https://bugzilla.redhat.com/show_bug.cgi?id=475631
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@179663 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

--- src/com/redhat/qe/auto/instantiate/VersionedInstantiator.java
+++ src/com/redhat/qe/auto/instantiate/VersionedInstantiator.java
@@ -18,26 +18,26 @@ import testopia.API.TestopiaObject;
 public class VersionedInstantiator {
 	protected static Logger log = Logger.getLogger(VersionedInstantiator.class.getName());
 	
-	protected Map<String,String> packageMap = new LinkedHashMap<String,String>();
+	protected LinkedHashMap<String,String> packageMap = new LinkedHashMap<String,String>();
 	protected Integer versionedPackageIndex = 1;
 	protected String runningVersion = "";
 	
 	/**
 	 * @param packageMap - a map of version strings to package names.  For example, a product version 2.2.0 might have 
-	 * a package com.xyz.product22.stuff.  You'd add an entry ("2.2.0", "product22") to the map.  You should add entries
+	 * a package com.xyz.product22.stuff.  You'd add an entry ("2.2.0", "product22") to the map.  You should use add entries
 	 * in increasing order (newer versions go last).
 	 * @param versionedPackageIndex - the index of where the versioned part of the package name is.  It's a 0 based index, 
 	 * so for package w.x.y.z.product22.a.b, the index is 4.
 	 * @param runningVersion - a valid key for the packageMap that indicates the currently running version.  This might
 	 * be detected at runtime from the product itself, or read in from a properties file.  For example "2.2.0".
 	 */
-	public VersionedInstantiator(Map<String,String> packageMap, Integer versionedPackageIndex, String runningVersion){
+	public VersionedInstantiator(LinkedHashMap<String,String> packageMap, Integer versionedPackageIndex, String runningVersion){
 		this.packageMap = packageMap;
 		this.versionedPackageIndex = versionedPackageIndex;
 		this.runningVersion = runningVersion;
 	}
 	
-	public  void setPackageMap(Map<String, String> packageMap){
+	public  void setPackageMap(LinkedHashMap<String, String> packageMap){
 		this.packageMap = packageMap;
 	}
 	
