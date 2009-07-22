9e89c03bf28e4e38b7b8f8b56618a4725156dac5
jweiss <jweiss@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2009-07-22 15:37:41 +0000

    Added TestNGHelper
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@185726 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

---
+++ src/com/redhat/qe/auto/testng/TestNGHelper.java
@@ -0,0 +1,21 @@
+package com.redhat.qe.auto.testng;
+
+import java.util.List;
+
+public class TestNGHelper {
+	public static Object[][] convertListOfListsTo2dArray(List<List<Object>> list) {
+		//convert list to 2-d array
+		Object[][] array = new Object[list.size()][ list.get(0).size()];
+		int i=0;
+		for (List<Object> item: list){
+			int j=0;
+			for (Object param: item){
+				array[i][j] = param;
+				j++;
+			}
+			i++;
+		}
+		return array;
+	  }
+
+}
