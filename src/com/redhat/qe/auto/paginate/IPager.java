1a9b05ff09917e09567797b0b63eacb6b69e7c0f
jsefler <jsefler@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2009-09-18 18:03:43 +0000

    Added a few interface methods" gotoFirstPage() gotoLastPage()
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@187157 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

--- src/com/redhat/qe/auto/paginate/IPager.java
+++ src/com/redhat/qe/auto/paginate/IPager.java
@@ -2,15 +2,21 @@ package com.redhat.qe.auto.paginate;
 
 public interface IPager {
 	
-	public void nextPage();
+	public boolean isFirstPage();
 	
-	public void previousPage();
+	public boolean isLastPage();
+	
+	public void goToNextPage();
+	
+	public void goToPriorPage();
+	
+	public void goToFirstPage();
+	
+	public void goToLastPage();
 	
 	public void goToPage(int page);
 	
 	public int getTotalNumberOfPages();
 
-	public boolean isLastPage();
-	
 	public int getTotalItems();
 }
