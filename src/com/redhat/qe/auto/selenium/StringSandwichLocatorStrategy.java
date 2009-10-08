c9cab9a73869a89dfa26138c414aa8aac123d1d2
jweiss <jweiss@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2009-10-08 16:00:45 +0000

    A locator strategy for a typical construction of locators:  const1 + arg1 + const2 + arg2...
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@187717 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

---
+++ src/com/redhat/qe/auto/selenium/StringSandwichLocatorStrategy.java
@@ -0,0 +1,42 @@
+package com.redhat.qe.auto.selenium;
+
+public class StringSandwichLocatorStrategy implements LocatorStrategy {
+
+	protected String[] bread = null;
+	protected String name = null;
+	
+	public StringSandwichLocatorStrategy(String name, String...bread){
+		this.name = name;
+		this.bread = bread;
+	}
+	
+	@Override
+	public String getLocator(String... meat) {
+		//construct the sandwich - bread, meat, bread, meat, bread - until one runs out
+		//always end with bread unless there isn't enough bread.  
+		//bread always comes first.
+		
+		int smallerStack = bread.length > meat.length ? meat.length : bread.length;
+		StringBuffer sb = new StringBuffer();
+		for (int i=0; i<smallerStack; i++){
+			sb.append(bread[i]);
+			sb.append(meat[i]);
+		}
+		if (bread.length > meat.length) sb.append(bread[meat.length]);
+		return sb.toString();
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	public static void main (String[] args){
+		StringSandwichLocatorStrategy ssls1 = new StringSandwichLocatorStrategy("pbj", "bread1", "bread2");
+		System.out.println(ssls1.getLocator("meat1"));
+		System.out.println(ssls1.getLocator("meat1", "meat2"));
+		System.out.println(ssls1.getLocator("meat1", "meat2", "meat3"));
+		
+	}
+
+}
