a83ec7dfa4bb113779ee3f6e8bb07aba92446300
whayutin <whayutin@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2009-10-28 17:09:32 +0000

    adding some basic helpers to allow one to run api tests against a xmlrpc server
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@188248 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

---
+++ src/com/redhat/qe/api/helper/TestHelper.java
@@ -0,0 +1,143 @@
+package com.redhat.qe.api.helper;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import redstone.xmlrpc.XmlRpcArray;
+import redstone.xmlrpc.XmlRpcStruct;
+
+
+public class TestHelper {
+	
+	/**
+	 * Prints a out an array to console
+	 * String array
+	 * @param array
+	 */
+	public void printArray(String[] array){
+		for(String str : array){
+			System.out.println(str);
+		}
+	}
+	
+	/**
+	 * Casts the object as a list array and prints
+	 * @param array
+	 */
+	public void printArray(Object array){
+		List myArray = (List) array;
+		 Iterator i = myArray.iterator();
+	        while (i.hasNext()) {
+	            System.out.println(i.next());
+	        }
+	}
+	
+	/*public void printOneRowOfArray(Object array, int start, int end){
+		List myArray = (List) array;
+	       List listSub = myArray.subList(start, end);
+	        System.out.println("sub"+listSub.toString());
+	}*/
+	
+	/**
+	 * prints a specific row of an array
+	 */
+	public void getRowOfArray(List array,int row){
+		//System.out.println("Object = " + array.getClass().toString());
+		System.out.println("getArray"+  array.get(row).toString());
+	}
+	
+	/**
+	 * Gets the keys from a List, and pulls the keys into an array and returns
+	 * @param results
+	 * @return ArrayList
+	 */
+	public ArrayList getKeys(List results){
+		Object o = (Object) results.get(0);
+		HashMap h = (HashMap) o;
+		Iterator i = h.keySet().iterator();
+		ArrayList<String> myKeys = new ArrayList<String>();
+		
+		while(i.hasNext()){
+			String key = (String) i.next();
+			myKeys.add(key);
+		}
+		return myKeys;	
+	}
+	
+	/**
+	 * Returns the key of a hashmap as an object
+	 * This is not really needed
+	 * @param results
+	 * @param keyString
+	 * @return
+	 * @deprecated
+	 */
+	private Object getKey(List results, String keyString){
+		Object keyObject = null;
+		Object o = (Object) results.get(0);
+		HashMap h = (HashMap) o;
+		Iterator i = h.keySet().iterator();
+		while(i.hasNext()){
+			Object tmp =  i.next();
+			if(keyString.equalsIgnoreCase(tmp.toString())){
+				keyObject = tmp;
+			}
+		}	
+		return keyObject;
+	}
+	
+	/**
+	 * Takes an array of objects or list and returns all the values of each for a given key
+	 * @param results
+	 * @param key
+	 * @return ArrayList
+	 */
+	public ArrayList getResultsForKey(List results,String key){
+		ArrayList<String> myResults = new ArrayList<String>();
+		for(Object obj : results){
+			HashMap hash = (HashMap) obj;
+			myResults.add(hash.get(key).toString());
+		}
+		return myResults;
+	}
+	
+	/**
+	 * Return a string of the value for a specificied key found by an associated know key and its known value
+	 * @param results
+	 * @param key
+	 * @param associatedKey
+	 * @param associatedKeyValue
+	 * @return String
+	 */
+	public String getValueOfKeyWithAttributes(List results, String key, String associatedKey, String associatedKeyValue){
+		String result = null;
+		for(Object obj : results){
+			HashMap hash = (HashMap) obj;
+			if((hash.get(associatedKey).toString()).equalsIgnoreCase(associatedKeyValue)){
+				result = hash.get(key).toString();
+			}
+		}
+		return result;	
+	}
+	
+	/**
+	 * If the value of the specified key matches the specified value, return the whole hash map 
+	 * @param results
+	 * @param key
+	 * @param value
+	 * @return HashMap
+	 */
+	public HashMap findHashFromListWithAttribute(List results, Object key, Object value){
+		for(Object obj : results){
+			HashMap hash = (HashMap) obj;
+			if(hash.get(key).equals(value))
+				return hash;
+		}
+		return null;
+	}
+	
+	
+}
---
+++ src/com/redhat/qe/api/helper/XmlRpcClientHelper.java
@@ -0,0 +1,84 @@
+package com.redhat.qe.api.helper;
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+
+
+import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import redstone.xmlrpc.*;
+
+/**
+ * CobblerConnection - represents an authenticatable 
+ * XMLRPC connection to Server.
+ */
+
+public class XmlRpcClientHelper {
+
+    private XmlRpcClient client;
+    private String token;
+
+    /**
+     * Constructor creates an authenticated connection to Server.
+     * read-only XMLRPC is not supported.
+     * @param url  Server XMLRPC endpoint, ex: http://server/api_path
+     * @param username Server XMLRPC username
+     * @param password Server XMLRPC password
+     * @throws XmlRpcException on remote or communication errors
+     */
+    
+    public XmlRpcClientHelper(String loginMethod,String url, String user, String pass) {
+        try {
+            client = new XmlRpcClient(url, false);
+        }
+        catch (MalformedURLException e) {
+            throw new XmlRpcException(e);
+        }
+        token = (String) invokeNoTokenMethod(loginMethod, user, pass);
+    }    
+    
+    /**
+     * Invoke an XMLRPC method.
+     * @param method method to invoke
+     * @param args args to pass to method
+     * @return Object data returned.
+     */
+    public Object invokeNoTokenMethod(String method, Object... args) {
+        try {
+            return client.invoke(method, Arrays.asList(args));
+        } 
+        catch (Exception e) {
+            throw new XmlRpcException("XmlRpcException calling Server.", e);
+        } 
+    }
+
+    /**
+     * Invoke an XMLRPC method.
+     * @param method method to invoke
+     * @param args args to pass to method
+     * @return Object data returned.
+     */
+    public Object invokeMethod(String method, Object ... args) {
+    	List params = new LinkedList(Arrays.asList(args));
+    	params.add(token);
+    	return invokeNoTokenMethod(method, params.toArray(new Object[0]));
+    }    
+    
+
+}
