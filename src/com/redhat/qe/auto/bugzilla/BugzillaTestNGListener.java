b62fbdfb70de2b9ed46fb7696956f16ea5bf4a10
jweiss <jweiss@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2009-10-30 13:54:33 +0000

    Added ability to skip blocked tests
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@188313 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

--- src/com/redhat/qe/auto/bugzilla/BugzillaTestNGListener.java
+++ src/com/redhat/qe/auto/bugzilla/BugzillaTestNGListener.java
@@ -6,11 +6,14 @@ import java.util.regex.Pattern;
 
 import org.testng.ITestContext;
 import org.testng.ITestResult;
+import org.testng.SkipException;
 import org.testng.internal.IResultListener;
 
 import com.redhat.qe.auto.testng.BzChecker;
 
 public class BugzillaTestNGListener implements IResultListener{
+	private static final String BLOCKED_BY_BUG = "blockedByBug";
+	private static final String VERIFIES_BUG = "verifiesBug";
 	protected static Logger log = Logger.getLogger(BugzillaTestNGListener.class.getName());
 	protected static BzChecker bzChecker = null;
 
@@ -63,9 +66,32 @@ public class BugzillaTestNGListener implements IResultListener{
 	}
 
 	@Override
-	public void onTestStart(ITestResult arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onTestStart(ITestResult result) {
+		/*
+		 * if the test is in a group "blockedByBug-xxxxxx" and the bug is not in
+		 * ON_QA, VERIFIED, RELEASE_PENDING, POST, CLOSED then skip it 
+		 */
+		if (bzChecker == null) {
+			bzChecker = new BzChecker();
+			bzChecker.init();
+		}
+		String[] groups = result.getMethod().getGroups();
+		Pattern p = Pattern.compile(BLOCKED_BY_BUG + "-(\\d+)");
+		for (String group: groups){
+			Matcher m = p.matcher(group);
+			if (m.find()){
+				String number = m.group(1);
+				BzChecker.bzState state = bzChecker.getBugState(number);
+				if (! (state.equals(BzChecker.bzState.ON_QA) ||
+						state.equals(BzChecker.bzState.VERIFIED) ||
+						state.equals(BzChecker.bzState.RELEASE_PENDING) ||
+						state.equals(BzChecker.bzState.POST) ||
+						state.equals(BzChecker.bzState.CLOSED))){
+					// the bug is not ready to retest
+					throw new SkipException("This test is blocked by bz bug " + number + ", which is currently " + state.toString());
+				}
+			}			
+		}
 	}
 
 	@Override
@@ -76,23 +102,34 @@ public class BugzillaTestNGListener implements IResultListener{
 		}
 		//if the test is in a group "verifiesBug-xxxxxx" and the bug is in ON_QA, close it
 		String[] groups = result.getMethod().getGroups();
-		Pattern p = Pattern.compile("verifiesBug-(\\d+)");
+		Pattern p = Pattern.compile("[" + VERIFIES_BUG + "|" + BLOCKED_BY_BUG +"]-(\\d+)");
 		for (String group: groups){
 			Matcher m = p.matcher(group);
 			if (m.find()){
 				String number = m.group(1);
-				log.fine("This test verifies bugzilla bug #"+ number);
 				BzChecker.bzState state = bzChecker.getBugState(number);
-				if (state.equals(BzChecker.bzState.ON_QA)){
-					//TODO need to call code here to actually close the bug (doesn't work yet)
-					log.warning("Need to verify bug " + number + "!");
-					bzChecker.setBugState(number, BzChecker.bzState.VERIFIED);
-					log.info("Verified bug " + number);
+				if (group.startsWith(VERIFIES_BUG)) {
+					log.fine("This test verifies bugzilla bug #"+ number);
+					if (state.equals(BzChecker.bzState.ON_QA)){
+						//TODO need to call code here to actually close the bug (doesn't work yet)
+						log.warning("Need to verify bug " + number + "!");
+						bzChecker.setBugState(number, BzChecker.bzState.VERIFIED);
+						log.info("Verified bug " + number);
+					}
+					else log.warning("Bug " + number + " has been verified, but it is in " + state + " state instead of ON_QA");
 				}
-				else log.warning("Bug " + number + " has been verified, but it is in " + state + " state instead of ON_QA");
+				else { //blockedByBug
+					log.warning("Test is now unblocked by bug " + number + ".");
+				}
 			}
-			
 		}
+	}
+	
+	public static void main (String... args) {
+		Pattern p = Pattern.compile("[verifiesBug|blockedByBug]-(\\d+)");
+		Matcher m = p.matcher("blockedByBug-12354542");
+		m.find();
+		System.out.println(m.group(1));
 	}
 
 }
