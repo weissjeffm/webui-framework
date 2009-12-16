7a7ed4fac097599eb8cdec3bb8db1b5b73ca166b
Jeff Weiss <jweiss@redhat.com>
2009-12-16 14:52:28 -0500

    refactored remotefiletasks

--- src/com/redhat/qe/tools/RemoteFileTasks.java
+++ src/com/redhat/qe/tools/RemoteFileTasks.java
@@ -1,5 +1,6 @@
 package com.redhat.qe.tools;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.redhat.qe.auto.selenium.MyLevel;
@@ -8,22 +9,19 @@ public class RemoteFileTasks {
 	
 	protected static Logger log = Logger.getLogger(RemoteFileTasks.class.getName());
 
-	public static int editFile (SSHCommandRunner runner, String filePath, String sedSearch, String sedReplace) {
-		runner.reset();
-		runner.setCommand("sed -i 's/"+sedSearch+"/"+sedReplace+"/g' " + filePath);
-		runner.run();
-		log.log(MyLevel.ACTION, "Running Command (on hostname "+runner.getConnection().getHostname()+"): " + runner.getCommand());
-		int returnCode = runner.waitFor();
-		log.fine("Stdout: "+runner.getStdout());
-		log.fine("Stderr: "+runner.getStderr());
-		return returnCode;
+	public static int searchReplaceFile (SSHCommandRunner runner, String filePath, String sedSearch, String sedReplace) {
+		return runCommand(runner, "sed -i 's/"+sedSearch+"/"+sedReplace+"/g' " + filePath, MyLevel.ACTION);
 	}
 	
 	public static int grepFile (SSHCommandRunner runner, String filePath, String searchTerm) {
+		return runCommand(runner, "grep " + searchTerm + " " + filePath, Level.INFO);
+	}
+	
+	public static int runCommand(SSHCommandRunner runner, String command, Level loglevel){
 		runner.reset();
-		runner.setCommand("grep " + searchTerm + " " + filePath);
+		runner.setCommand(command);
 		runner.run();
-		log.info("Running Command (on hostname "+runner.getConnection().getHostname()+"): " + runner.getCommand());
+		log.log(loglevel, "Running Command (on hostname "+runner.getConnection().getHostname()+"): " + runner.getCommand());
 		int returnCode = runner.waitFor();
 		log.fine("Stdout: "+runner.getStdout());
 		log.fine("Stderr: "+runner.getStderr());
