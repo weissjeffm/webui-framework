e805394e3774326788aea12a5168fb84b648db86
John Sefler <jsefler@redhat.com>
2010-02-19 09:26:11 -0500

    avoid null pointer exceptions when list is empty

--- src/com/redhat/qe/auto/testng/TestNGUtils.java
+++ src/com/redhat/qe/auto/testng/TestNGUtils.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class TestNGUtils {
 	
 	public static Object[][] convertListOfListsTo2dArray(List<List<Object>> list) {
+		if (list.size() == 0) return new Object[0][0]; // avoid a null pointer exception
+		
 		//convert list to 2-d array
 		Object[][] array = new Object[list.size()][ list.get(0).size()];
 		int i=0;
