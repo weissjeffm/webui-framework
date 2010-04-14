70b8bbdce177efd801e294f74b95ea15aed1458b
Jeff Weiss <jweiss@redhat.com>
2010-04-14 15:54:06 -0400

    added comma to list of valid url chars

--- src/com/redhat/qe/auto/testng/TestNGReportHandler.java
+++ src/com/redhat/qe/auto/testng/TestNGReportHandler.java
@@ -49,7 +49,7 @@ public class TestNGReportHandler extends Handler {
 	 */
 	protected static String tagAllUrls(String msg) {
 		//String regex = "(http[s]?://[\\w\\d:/.$~\\-_?=&%#]+)";
-		String regex = "((http[s]?|ftp|gopher|telnet|file|notes|ms-help):(//)[\\w\\d:/.$~\\-_?=&%#;]+)"; // ((http[s]?|ftp|gopher|telnet|file|notes|ms-help):(//)[\w\d:/.$~\-_?=&%#;]+)
+		String regex = "((http[s]?|ftp|gopher|telnet|file|notes|ms-help):(//)[\\w\\d:/.,$~\\-_?=&%#;]+)"; // ((http[s]?|ftp|gopher|telnet|file|notes|ms-help):(//)[\w\d:/.$~\-_?=&%#;]+)
 		return msg.replaceAll(regex, "<a href=$1>$1</a>");
 	}
 	
