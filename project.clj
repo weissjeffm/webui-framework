64b591dc62b955d2528d5dc4514fea4c0f5c3dd7
Jeff Weiss <jweiss@redhat.com>
2010-12-06 15:02:38 -0500

    trying leiningen for build tool

--- project.clj
+++ project.clj
@@ -4,5 +4,12 @@
   :java-options {:debug "true" }
   :repositories {"apacherepository" "http://repository.apache.org/snapshots/"}
   :dependencies [[org.apache.xmlrpc/xmlrpc-client "3.1.3"]
-		 [org.clojars.kjw/commons-httpclient "3.1"] ]
-  :dev-dependencies [ [lein-javac "1.2.1-SNAPSHOT"] ])
+		 [org.clojars.kjw/commons-httpclient "3.1"]
+		 [com.google.collections/google-collections "1.0"]
+		 [velocity/velocity "1.4"]
+		 [org.testng/testng "5.14.2"]
+		 [com.trilead/trilead-ssh2 "build213-svnkit-1.3-patch"]
+		 [ca.juliusdavies/not-yet-commons-ssl "0.3.11"]
+		 [org.clojars.weissjeffm/httpclient-negotiate "1.0"]
+		 [org.seleniumhq.selenium.client-drivers/selenium-java-client-driver "1.0.2"]]
+  :dev-dependencies [ [lein-javac "1.2.1-SNAPSHOT"] [lein-eclipse "1.0.0"] ])
