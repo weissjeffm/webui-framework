50399f044e3e766545f7334a6dfc77a2d09505ef
John Sefler <jsefler@redhat.com>
2011-01-06 10:32:22 -0500

    added comments to TabElement.java

--- src/com/redhat/qe/auto/selenium/TabElement.java
+++ src/com/redhat/qe/auto/selenium/TabElement.java
@@ -1,8 +1,16 @@
 package com.redhat.qe.auto.selenium;
 
+/**
+ * A Tab Element represents a gui control that contains two states (typically selected and unselected).
+ */
 public class TabElement extends Element{
 	private LocatorStrategy alternateLocatorStrategy = null;
 
+	/**
+	 * @param locatorStrategy - this strategy should resolve to the selenium locator needed to click an existing tab in its unselected state
+	 * @param alternateLocatorStrategy - this strategy should resolve to the selenium locator that identifies an existing tab after it has been selected and is now in its selected state
+	 * @param locatorStrategyArgs - arguments fed to both the locatorStrategy and alternateLocatorStrategy to create the selenium locators
+	 */
 	public TabElement(LocatorStrategy locatorStrategy,  LocatorStrategy alternateLocatorStrategy, String... locatorStrategyArgs) {
 		this.locatorStrategy = locatorStrategy;
 		
@@ -10,6 +18,10 @@ public class TabElement extends Element{
 		this.locatorStrategyArgs = locatorStrategyArgs;
 	}
 	
+	/**
+	 * This constructor is only used when the the same locator can be used to identify both the selected and unselected state of the gui control.
+	 * @param locator
+	 */
 	public TabElement(String locator){
 		this.locator = locator;
 	}
