edaaf7a2faaeaf4917a1ea5ec5569059fd1c8d60
Jeff Weiss <jweiss@redhat.com>
2011-02-07 15:07:04 -0500

    added selenium functionality and config is now factored out

--- src/com/redhat/qe/auto/selenium/selenium.clj
+++ src/com/redhat/qe/auto/selenium/selenium.clj
@@ -1,10 +1,16 @@
 (ns com.redhat.qe.auto.selenium.selenium
   (:import [com.redhat.qe.auto.selenium ExtendedSelenium]))
 
+(defprotocol SeleniumLocatable
+  (sel-locator [x]))
+
 (def sel (atom nil))
 
 (defn connect [host port browser-type url]
   (reset! sel (ExtendedSelenium. host port browser-type url)))
 
 (defmacro browser [action & args]
-  `(clojure.lang.Reflector/invokeInstanceMethod (deref sel) ~(str action) (into-array (quote ~args))))
+  `(let [locator-args# (for [arg# [~@args]]
+                       (if (keyword? arg#) (sel-locator arg#) arg#))]
+    (clojure.lang.Reflector/invokeInstanceMethod
+     (deref sel) ~(str action) (into-array locator-args#))))
