04b58730c7f3f40cc662a2afe129e75f4f6ece11
Jeff Weiss <jweiss@redhat.com>
2011-02-16 11:31:20 -0500

    better error when ui map key is not found

--- src/com/redhat/qe/auto/selenium/selenium.clj
+++ src/com/redhat/qe/auto/selenium/selenium.clj
@@ -1,16 +1,26 @@
 (ns com.redhat.qe.auto.selenium.selenium
-  (:import [com.redhat.qe.auto.selenium ExtendedSelenium]))
+  (:import [com.redhat.qe.auto.selenium ExtendedSelenium Element LocatorTemplate]))
 
 (defprotocol SeleniumLocatable
   (sel-locator [x]))
 
 (def sel (atom nil))
 
+
 (defn connect [host port browser-type url]
   (reset! sel (ExtendedSelenium. host port browser-type url)))
 
+(defn new-element [locator-strategy & args]
+  (Element. locator-strategy (into-array args)))
+
 (defmacro browser [action & args]
   `(let [locator-args# (for [arg# [~@args]]
-                       (if (keyword? arg#) (sel-locator arg#) arg#))]
+                         (if (keyword? arg#)
+                           (let [locator# (sel-locator arg#)]
+                             (if (nil? locator#)
+                               (throw (IllegalArgumentException.
+                                       (str "Locator " arg# " not found in UI mapping.")))
+                               locator#))
+                           arg#))]
     (clojure.lang.Reflector/invokeInstanceMethod
-     (deref sel) ~(str action) (into-array locator-args#))))
+     (deref sel) ~(str action) (into-array Object locator-args#))))
