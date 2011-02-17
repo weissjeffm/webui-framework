d3ee1d2baa9937923831ad586fe992c884f57bb5
Jeff Weiss <jweiss@redhat.com>
2011-02-17 15:24:21 -0500

    I think i fixed the NPE this time

--- src/com/redhat/qe/auto/navigate.clj
+++ src/com/redhat/qe/auto/navigate.clj
@@ -11,30 +11,41 @@
         linkmap (if (nil? links) {} {:links (vec links)})]
     (merge m args linkmap)))
 
-(defn page-zip [tree] (zip/zipper #(contains? % :links)
-                          #(:links %)
+(defn page-zip [tree] (zip/zipper (constantly true)
+                          #(or (:links %) [])
                           #(conj %1 {:links %2})
                           tree))
 
 (defn find-node [z pred]
-  (->> (iterate zip/next z)
-       (take-while #(not (zip/end? %)))
-       (filter #(pred (zip/node %)))
-       first))
+  (println (str "yo " z))
+  (and z (->> (iterate zip/next z)
+              (take-while #(not (zip/end? %1)))
+              (some  #(when (pred (zip/node %)) %)))))
 
 (defn page-path [page z]
-  (let [first-match (find-node z #(= (:page %) page))]
+  (let [page-match? #(= (:page %) page)
+        first-match (find-node z page-match?)]
     (if (nil? first-match)
       (throw (NoSuchElementException. (str "Page " page " was not found in navigation tree."))))
     (conj (zip/path first-match) (zip/node first-match))))
 
-(defn navigate [page z args]
-  (let [path (page-path page z)
-        all-req-args (set (mapcat :req-args path))
-        missing-args (clojure.set/difference all-req-args (set (keys args)))]
-    (if-not (zero? (count missing-args))
-      (throw (IllegalArgumentException. (str "Missing required keys to navigate to " page " - " missing-args)))
-      (for [step path]
-        (apply (:fn step)
-               (for [req-arg (:req-args step)]
-                 (req-arg args)))))))
+(defn navigate
+  ([page z args]
+     (let [path (page-path page z)
+           all-req-args (set (mapcat :req-args path))
+           missing-args (clojure.set/difference all-req-args (set (keys args)))]
+       (if-not (zero? (count missing-args))
+         (throw (IllegalArgumentException. (str "Missing required keys to navigate to " page " - " missing-args)))
+         (for [step path]
+           (apply (:fn step)
+                  (for [req-arg (:req-args step)]
+                    (req-arg args)))))))
+  ([page z] (navigate page z {})))
+
+(defn nav-fn "Closes over a page tree structure and returns a navigation function"
+  [tree]
+  (let [z (page-zip tree)]
+   (fn
+     ([page args]
+        (navigate page z args))
+     ([page] (navigate page z {})))))
