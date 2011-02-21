b6930957becb23ef74d742e113c55293faefeb65
Jeff Weiss <jweiss@redhat.com>
2011-02-21 18:04:17 -0500

    verify macro handles exceptions better

--- src/com/redhat/qe/verify.clj
+++ src/com/redhat/qe/verify.clj
@@ -12,11 +12,16 @@
   "Evaluates expr and either logs what was evaluated, or throws an exception if it does not evaluate to logical true."
   [x]
   (let [bindings (local-bindings &env)]
-    `(let [res# ~x
+    `(let [pass# (atom false)
+           res# (try (do ~x (reset! pass# true)) (catch Exception e# e#))
            sep#  (System/getProperty "line.separator")
            form# '~x
            used-bindings# (select-keys ~bindings (distinct (flatten form#)))
-           msg# (apply str (if res# "Verified: " "Verification failed: ") (pr-str form#) sep#
+           msg# (apply str (if (and @pass# res#) "Verified: " "Verification failed: ") (pr-str form#) sep#
                        (map (fn [[k# v#]] (str "\t" k# " : " v# sep#)) 
                             used-bindings#))]
-       (if res# (log/info msg#) (throw (AssertionError. msg#))))))
+       (if (and @pass# res#) (log/info msg#)
+           (let [err# (AssertionError. msg#)]
+             (throw (if (and res# (not @pass#))
+                      (.initCause err# res#))
+                    err#))))))
