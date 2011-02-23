15144b5f8e6ea29ae94d7c09f402f974e1849670
Jeff Weiss <jweiss@redhat.com>
2011-02-22 20:59:24 -0500

    fixed error in verify macro

--- src/com/redhat/qe/verify.clj
+++ src/com/redhat/qe/verify.clj
@@ -12,16 +12,16 @@
   "Evaluates expr and either logs what was evaluated, or throws an exception if it does not evaluate to logical true."
   [x]
   (let [bindings (local-bindings &env)]
-    `(let [pass# (atom false)
-           res# (try (do ~x (reset! pass# true)) (catch Exception e# e#))
+    `(let [noerr# (atom true)
+           res# (try ~x (catch Exception e# (do (reset! noerr# false) e#)))
            sep#  (System/getProperty "line.separator")
            form# '~x
            used-bindings# (select-keys ~bindings (distinct (flatten form#)))
-           msg# (apply str (if (and @pass# res#) "Verified: " "Verification failed: ") (pr-str form#) sep#
+           msg# (apply str (if (and @noerr# res#) "Verified: " "Verification failed: ") (pr-str form#) sep#
                        (map (fn [[k# v#]] (str "\t" k# " : " v# sep#)) 
                             used-bindings#))]
-       (if (and @pass# res#) (log/info msg#)
+       (if (and @noerr# res#) (log/info msg#)
            (let [err# (AssertionError. msg#)]
-             (throw (if (and res# (not @pass#))
-                      (.initCause err# res#))
-                    err#))))))
+             (throw (if (and res# (not @noerr#))
+                      (.initCause err# res#)
+                      err#)))))))
