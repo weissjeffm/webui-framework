a27ba7a8c3d1bee7ff6cb985519ed942b7adab6e
jmolet <jmolet@redhat.com>
2011-03-16 17:10:06 -0400

    chaning args to beakercheckout

--- scripts/beakerCheckOut.sh
+++ scripts/beakerCheckOut.sh
@@ -12,66 +12,85 @@
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
 #
-# written by whayutin@redhat.com
+# written by whayutin@redhat.com & jmolet@redhat.com
 
 
 function usage()
 {
-           echo " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " 
-	   echo "Please use all options"
-	   echo ""
+           echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 
+	         echo "Please use all options"
+	         echo ""
            echo " This script will reserve a beaker box  "
-           echo " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " 
+           echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 
            echo ""
-           echo "Available options are:"
-           echo "--ARCH=             :: i386, x86_64 etc."
-           echo "--FAMILY=           :: DISTRO FAMILY, RedHatEnterprixeLinux4/5/6"
-           echo "--TAG=              :: TAG, RELEASED,STABLE"  
-           echo "--VARIANT=          :: RHEL6 has Server,Workstation,ComputeNode,Client"
-           echo "--TASK=             :: Additional beaker task executed after provision"       
+           echo "Arguments will be passed to bkr workflow-simple.  Available options are:"
+           echo "--username=         :: Your beaker username."
+           echo "--password=         :: Your beaker password."
+           echo "--arch=             :: i386, x86_64 etc."
+           echo "--family=           :: DISTRO FAMILY, RedHatEnterprixeLinux4/5/6"
+           echo "--tag=              :: TAG, RELEASED,STABLE"  
+           echo "--variant=          :: RHEL6 has Server,Workstation,ComputeNode,Client"
+           echo "--task=             :: Additional beaker task executed after provision"       
 }
 
-#cli
+USERNAME=""
+PASSWORD=""
+ARCH=""
+FAMILY=""
+TAG=""
+VARIANT=""
+TASK=""
+
 for i in $*
  do
  case $i in
-      --ARCH=*)
-         ARCH="`echo $i | sed 's/[-a-zA-Z0-9]*=//'`"
+      --username=*)
+         echo "Setting arg: $i"
+         USERNAME=$i
          ;;
-      --FAMILY=*)
-         FAMILY="`echo $i | sed 's/[-A-Z]*=//'`"
+      --password=*)
+         echo "Setting password"
+         PASSWORD=$i
          ;;
-      --TAG=*)
-          TAG="`echo $i | sed 's/[-a-zA-Z]*=//'`"
+      --arch=*)
+         echo "Setting arg: $i"
+         ARCH=$i
+         ;;
+      --family=*)
+        echo "Setting arg: $i"
+        FAMILY=$i
+         ;;
+      --tag=*)
+        echo "Setting arg: $i"
+        TAG=$i
           ;;
-      --VARIANT=*)
-          VARIANT="`echo $i | sed 's/[-a-zA-Z]*=//'`"
+      --variant=*)
+        echo "Setting arg: $i"
+        VARIANT=$i
           ;;
-      --TASK=*)
-          TASK="`echo $i | sed 's/[-a-zA-Z]*=//'`"
+      --task=*)
+        echo "Adding arg to tasks: $i"
+        TASK=${TASK}" "$i
           ;;
-        *)
-         # unknown option 
-	   usage
-           exit 1
-           ;;
+      *)
+        echo "Unknown Option: $i"
+         usage
+        exit 1
+        ;;
  esac
 done
 
-echo "ARCH = $ARCH"
-echo "FAMILY = $FAMILY"
-echo "TAG = $TAG"
 
-if [[ -z $ARCH ]] || [[ -z $FAMILY ]] || [[ -z $TAG ]] || [[ -z $VARIANT ]] || [[ -z $TASK ]]  ; then
+if [[ -z $USERNAME ]] || [[ -z $PASSWORD ]] || [[ -z $ARCH ]] || [[ -z $FAMILY ]] || [[ -z $TAG ]] || [[ -z $VARIANT ]] || [[ -z $TASK ]]  ; then
  usage
  exit 1
 fi
 
 
-bkr workflow-simple --username=cloudqe --password=pcx7MOdy1 --arch=$ARCH --variant=$VARIANT --family=$FAMILY --tag=$TAG --task=$TASK  --task=/distribution/reservesys > job || (echo "bkr workflow-simple --username=cloudqe --password=pcx7MOdy1 --arch=$ARCH --variant=$VARIANT --family=$FAMILY --tag=$TAG --task=$TASK --task=/distribution/reservesys " && cat job && exit 1)
+bkr workflow-simple $USERNAME $PASSWORD $ARCH $FAMILY $TAG $VARIANT $TASK --task=/distribution/reservesys > job || (echo "bkr workflow-simple $USERNAME --password=***** $ARCH $FAMILY $TAG $VARIANT $TASK --task=/distribution/reservesys " && cat job && exit 1)
 
 echo "===================== JOB DETAILS ================"
-echo "bkr workflow-simple --username=cloudqe --password=****** --arch=$ARCH --variant=$VARIANT --family=$FAMILY --tag=$TAG --task=$TASK --task=/distribution/reservesys "
+echo "bkr workflow-simple $USERNAME --password=***** $ARCH $FAMILY $TAG $VARIANT $TASK --task=/distribution/reservesys "
 cat job
 echo "===================== JOB DETAILS ================"
 JOB=`cat job | cut -d \' -f 2`
@@ -83,14 +102,14 @@ echo "===================== JOB ID ================"
 pwd
 
 
-bkr job-results $JOB --username=cloudqe --password=Q05Syutri > job-result
+bkr job-results $JOB $USERNAME $PASSWORD > job-result
 PROVISION_RESULT=`xmlstarlet sel -t --value-of "//task[@name='/distribution/install']/@result" job-result`
 PROVISION_STATUS=`xmlstarlet sel -t --value-of "//task[@name='/distribution/install']/@status" job-result`
 echo "===================== PROVISION STATUS ================"
 PREV_STATUS="Hasn't Started Yet."
 while [ $PROVISION_RESULT != "Pass" ] || [ $PROVISION_RESULT != "Warn" ];
 do
- bkr job-results $JOB --username=cloudqe --password=Q05Syutri > job-result
+ bkr job-results $JOB $USERNAME $PASSWORD > job-result
  PROVISION_RESULT=$(xmlstarlet sel -t --value-of "//task[@name='/distribution/install']/@result" job-result)
  PROVISION_STATUS=$(xmlstarlet sel -t --value-of "//task[@name='/distribution/install']/@status" job-result)
  if [ "$PREV_STATUS" == "$PROVISION_STATUS" ]; then
@@ -139,7 +158,7 @@ echo "===================== AUTOMATION PREREQ STATUS ================"
 PREV_STATUS="Hasn't Started Yet."
 while [ $SETUP_RESULT != "Pass" ] || [ $SETUP_RESULT != "Warn" ];
 do
- bkr job-results $JOB --username=cloudqe --password=Q05Syutri > job-result
+ bkr job-results $JOB $USERNAME $PASSWORD > job-result
  SETUP_RESULT=$(xmlstarlet sel -t --value-of "//task[@name='/CoreOS/rhsm/Install/subscription-manager-env']/@result" job-result)
  SETUP_STATUS=$(xmlstarlet sel -t --value-of "//task[@name='/CoreOS/rhsm/Install/subscription-manager-env']/@status" job-result)
  if [ "$PREV_STATUS" == "$SETUP_STATUS" ]; then
