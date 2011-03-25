6de2c675fb2aa0e459d85108bdf9ecffa584dd49
jmolet <jmolet@redhat.com>
2011-03-25 12:20:40 -0400

    adding timeout and new arg functionality to beakerCheckOut

--- scripts/beakerCheckOut.sh
+++ scripts/beakerCheckOut.sh
@@ -14,80 +14,113 @@
 #
 # written by whayutin@redhat.com & jmolet@redhat.com
 
-
 function usage()
 {
-           echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 
-	         echo "Please use all options"
-	         echo ""
-           echo " This script will reserve a beaker box  "
-           echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 
-           echo ""
-           echo "Arguments will be passed to bkr workflow-simple.  Available options are:"
-           echo "--username=         :: Your beaker username."
-           echo "--password=         :: Your beaker password."
-           echo "--arch=             :: i386, x86_64 etc."
-           echo "--family=           :: DISTRO FAMILY, RedHatEnterprixeLinux4/5/6"
-           echo "--tag=              :: TAG, RELEASED,STABLE"  
-           echo "--variant=          :: RHEL6 has Server,Workstation,ComputeNode,Client"
-           echo "--task=             :: Additional beaker task executed after provision"       
+  echo "This script will reserve a beaker box using bkr workflow-simple."
+  echo 
+  echo "Available options are:"
+  echo "--help                                     Prints this message and then exits"
+  echo "--timeout=TIMEOUT                          The timeout in minutes to wait for a beaker box (default: 180)"
+  echo 
+  echo "The following options are avalable to bkr workflow-simple:"
+  echo "--username=USERNAME                        specify user"
+  echo "--password=PASSWORD                        specify password"
+  echo "--prettyxml                                print the xml in pretty format"
+  echo "--debug                                    print the jobxml that it would submit"
+  echo "--dryrun                                   Don't submit job to scheduler"
+  echo "--arch=ARCH                                Include this Arch in job"
+  echo "--distro=DISTRO                            Use this Distro for job"
+  echo "--family=FAMILY                            Pick latest distro of this family for job"
+  echo "--variant=VARIANT                          Pick distro with this variant for job"
+  echo "--machine=MACHINE                          Require this machine for job"
+  echo "--package=PACKAGE                          Include tests for Package in job"
+  echo "--tag=TAG                                  Pick latest distro matching this tag for job"
+  echo "--retention_tag=RETENTION_TAG              Specify data retention policy for this job, defaults to Scratch"
+  echo "--repo=REPO                                Include this repo in job"
+  echo "--task=TASK                                Include this task in job"
+  echo "--taskparam=TASKPARAM                      Set task params 'name=value'"
+  echo "--type=TYPE                                Include tasks of this type in job"
+  echo "--systype=SYSTYPE                          Specify the System Type (Machine, Laptop, etc..)"
+  echo "--keyvalue=KEYVALUE                        Specify a system that matches this key/value Example: NETWORK=e1000"
+  echo "--whiteboard=WHITEBOARD                    Set the whiteboard for this job"
+  echo "--wait                                     wait on job completion"
+  echo "--nowait                                   Do not wait on job completion [Default]"
+  echo "--clients=CLIENTS                          Specify how many client hosts to be involved in multihost test"
+  echo "--servers=SERVERS                          Specify how many server hosts to be involved in multihost test"
+  echo "--install=INSTALL                          Specify Package to install, this will add /distribution/pkginstall."
+  echo "--cc=CC                                    Specify additional email addresses to notify"
+  echo "--dump                                     Turn on ndnc/kdump. (which one depends on the family)"
+  echo "--method=METHOD                            Installation source method (nfs/http) (optional)"
+  echo "--priority=PRIORITY                        Set the priority to this (Low,Medium,Normal,High,Urgent) (optional)"
+  echo "--kernel_options=KERNEL_OPTIONS            Boot arguments to supply (optional)"
+  echo "--kernel_options_post=KERNEL_OPTIONS_POST  Boot arguments to supply post install (optional)"
+  echo "--product=PRODUCT                          This should be a unique identifierf or a product"   
 }
 
+TIMEOUT="180"
 USERNAME=""
 PASSWORD=""
 ARCH=""
 FAMILY=""
-TAG=""
-VARIANT=""
-TASK=""
+TASKS=""
+OTHERARGS=""
 
 for i in $*
  do
  case $i in
+      --help)
+         usage
+         exit 0
+         ;;
+      --timeout=*)
+         TIMEOUT=$(echo $i | sed -e s/--timeout=//g)
+         ;; 
       --username=*)
-         echo "Setting arg: $i"
+         echo "Setting Arg: $i"
          USERNAME=$i
          ;;
       --password=*)
-         echo "Setting password"
+         echo "Setting Password."
          PASSWORD=$i
          ;;
       --arch=*)
-         echo "Setting arg: $i"
+         echo "Setting Arg: $i"
          ARCH=$i
          ;;
       --family=*)
-        echo "Setting arg: $i"
+        echo "Setting Arg: $i"
         FAMILY=$i
          ;;
-      --tag=*)
-        echo "Setting arg: $i"
-        TAG=$i
-          ;;
-      --variant=*)
-        echo "Setting arg: $i"
-        VARIANT=$i
-          ;;
       --task=*)
         echo "Adding arg to tasks: $i"
-        TASK=${TASK}" "$i
-          ;;
+        TASKS=${TASKS}" "$i
+        ;;
       *)
-        echo "Unknown Option: $i"
-         usage
-        exit 1
+        echo "Adding $i to other bkr workflow-simple args."
+        OTHERARGS=${OTHERARGS}" "$i
         ;;
  esac
 done
 
+#echo "args: $@"
+#echo "USERNAME: $USERNAME"
+#echo "PASSWORD: $PASSWORD"
+#echo "ARCH: $ARCH"
+#echo "FAMILY: $FAMILY"
+#echo "TASKS: $TASKS"
+#echo "OTHERARGS: $OTHERARGS"
+#echo "TIMEOUT: $TIMEOUT"
 
-if [[ -z $USERNAME ]] || [[ -z $PASSWORD ]] || [[ -z $ARCH ]] || [[ -z $FAMILY ]] || [[ -z $TASK ]]  ; then
+
+if [[ -z $USERNAME ]] || [[ -z $PASSWORD ]] || [[ -z $ARCH ]] || [[ -z $FAMILY ]] || [[ -z $TASKS ]]  ; then
+ echo "bkr workflow-simple requires that a username, password, arch, family, and task be given."
+ echo
  usage
  exit 1
 fi
 
 
-bkr workflow-simple $USERNAME $PASSWORD $ARCH $FAMILY $TAG $VARIANT $TASK --task=/distribution/reservesys > job || (echo "bkr workflow-simple $USERNAME --password=***** $ARCH $FAMILY $TAG $VARIANT $TASK --task=/distribution/reservesys " && cat job && exit 1)
+bkr workflow-simple $USERNAME $PASSWORD $ARCH $FAMILY $TASKS --task=/distribution/reservesys $OTHERARGS > job || (echo "bkr workflow-simple $USERNAME --password=***** $ARCH $FAMILY $TASK --task=/distribution/reservesys $OTHERARGS " && cat job && exit 1)
 
 echo "===================== JOB DETAILS ================"
 echo "bkr workflow-simple $USERNAME --password=***** $ARCH $FAMILY $TAG $VARIANT $TASK --task=/distribution/reservesys "
@@ -106,8 +139,10 @@ bkr job-results $JOB $USERNAME $PASSWORD > job-result
 PROVISION_RESULT=`xmlstarlet sel -t --value-of "//task[@name='/distribution/install']/@result" job-result`
 PROVISION_STATUS=`xmlstarlet sel -t --value-of "//task[@name='/distribution/install']/@status" job-result`
 echo "===================== PROVISION STATUS ================"
+echo "Timeout: $TIMEOUT minutes"
 PREV_STATUS="Hasn't Started Yet."
-while [ true ]; do
+TIME="0"
+while [ $TIME -lt $TIMEOUT ]; do
   bkr job-results $JOB $USERNAME $PASSWORD > job-result
   PROVISION_RESULT=$(xmlstarlet sel -t --value-of "//task[@name='/distribution/install']/@result" job-result)
   PROVISION_STATUS=$(xmlstarlet sel -t --value-of "//task[@name='/distribution/install']/@status" job-result)
@@ -126,6 +161,7 @@ while [ true ]; do
     break
   elif [ "$PREV_STATUS" == "$PROVISION_STATUS" ]; then
     echo -n "."
+    $TIME=$(expr $TIME + 1)
     sleep 60
   else 
    echo
@@ -133,12 +169,16 @@ while [ true ]; do
    echo "Provision Result: $PROVISION_RESULT"
    date
    PREV_STATUS=$PROVISION_STATUS
+   echo "Timeout timer reset."
+   $TIME="0"
    sleep 60
   fi
 done
+if [[ $TIME -eq $TIMEOUT ]]; then
+  echo "Timeout reached."
+  exit 1
+fi
 echo "===================== PROVISION STATUS ================"
-
-
 
 
 SETUP_RESULT=`xmlstarlet sel -t --value-of "//task[@name='/CoreOS/rhsm/Install/subscription-manager-env']/@result" job-result`
