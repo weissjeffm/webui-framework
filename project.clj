d6bc0425cbc17a6efc3ae5b514f90970290b559b
Jeff Weiss <jweiss@redhat.com>
2011-04-05 10:47:26 -0400

    upgrade to testng 6

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject webui-framework "1.0.1-SNAPSHOT"
+(defproject webui-framework "1.0.2-SNAPSHOT"
   :description "A library of automated testing related functions."
   :java-source-path "src"
   :dependencies [[org.apache.xmlrpc/xmlrpc-client "3.1.3"]
 		 [commons-httpclient/commons-httpclient "3.1"]
 		 [com.google.collections/google-collections "1.0"]
-		 [org.testng/testng "[5.14,5.15)"]
+		 [org.testng/testng "[6.0,7.0)"]
 		 [com.trilead/trilead-ssh2 "build213-svnkit-1.3-patch"]
 		 [ca.juliusdavies/not-yet-commons-ssl "0.3.11"]
 		 [org.clojars.weissjeffm/httpclient-negotiate "1.0"]
