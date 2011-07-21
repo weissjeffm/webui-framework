47f9bbbcd30b4bcf554d5bbd322eb72b39917483
Jeff Weiss <jweiss@redhat.com>
2011-07-21 12:03:25 -0400

    add small clojure bz checker

---
+++ src/com/redhat/qe/auto/bz.clj
@@ -0,0 +1,9 @@
+(ns com.redhat.qe.auto.bz
+  (:import [com.redhat.qe.auto.testng BzChecker]))
+
+(defn blocked-by-bz-bugs [ & ids]
+  (fn [_]
+    (let [checker (BzChecker/getInstance)
+          still-open (filter (fn [id] (.isBugOpen checker id)) ids)]
+      (if (= 0 (count still-open)) nil
+          still-open))))
