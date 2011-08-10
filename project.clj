bf6f86d5c2cae2ef09ccf5b80c0ae57038e5f18f
Jeff Weiss <jweiss@redhat.com>
2011-08-10 15:21:11 -0400

    remove unnecessary dev deps

--- project.clj
+++ project.clj
@@ -12,7 +12,4 @@
 		 [org.seleniumhq.selenium.client-drivers/selenium-java-client-driver "1.0.2"]
                  [org.uncommons/reportng "1.1.3"]
                  [sahi "3.5"]]
-  :dev-dependencies [[org.clojure/clojure "1.2.0"]
-                     [org.clojure/clojure-contrib "1.2.0"]
-                     [swank-clojure "1.2.1"]]
   :javac-options {:debug "on"})
