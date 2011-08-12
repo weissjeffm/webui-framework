53fb23d3c5725ab08379ec9ee19851688c370243
Jeff Weiss <jweiss@redhat.com>
2011-08-12 14:52:12 -0400

    have blocked by bz bugs print out prettier

--- src/com/redhat/qe/auto/bz.clj
+++ src/com/redhat/qe/auto/bz.clj
@@ -2,9 +2,14 @@
   (:import [com.redhat.qe.auto.testng BzChecker]))
 
 (defn blocked-by-bz-bugs [ & ids]
-  (fn [_]
-    (let [checker (BzChecker/getInstance)
-          still-open (filter (memoize (fn [id] (.isBugOpen checker id)))
-                             ids)]
-      (if (= 0 (count still-open)) nil
-          still-open))))
+  (with-meta (fn [_]
+               (let [checker (BzChecker/getInstance)
+                     still-open (filter (memoize (fn [id] (.isBugOpen checker id)))
+                                        ids)]
+                 (if (= 0 (count still-open)) nil
+                     still-open)))
+    {:type :bz-blocker
+     ::source `(~'blocked-by-bz-bugs ~@ids)}))
+
+(defmethod print-method :bz-blocker [o ^Writer w]
+  (print-method (::source (meta o)) w))
