d5e7a34d62c79fc8d0a098e454c9db200a5a01b6
Jeeva Kandasamy <jkandasa@redhat.com>
2011-09-06 17:46:32 +0530

    Adding convenience methods

--- src/com/redhat/qe/auto/sahi/ExtendedSahi.java
+++ src/com/redhat/qe/auto/sahi/ExtendedSahi.java
@@ -1,8 +1,12 @@
 package com.redhat.qe.auto.sahi;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import net.sf.sahi.client.Browser;
+import net.sf.sahi.client.ElementStub;
 import net.sf.sahi.config.Configuration;
 
 /**
@@ -12,10 +16,101 @@ import net.sf.sahi.config.Configuration;
  * @author dgao
  */
 public class ExtendedSahi extends Browser {
-	private static Logger log = Logger.getLogger(ExtendedSahi.class.getName());
+	private static Logger _logger = Logger.getLogger(ExtendedSahi.class.getName());
 
 	public ExtendedSahi(String browserPath, String browserProcessName, String browserOpt, String sahiDir, String userDataDir) {
 		super(browserPath, browserProcessName, browserOpt);
 		Configuration.initJava(sahiDir, userDataDir);
+	}
+	
+	//This method is used to select drop down on GWT web (Example- RHQ 4.x)
+	public void selectComboBoxDivRow(Browser browser, String comboBoxIdentifier, String optionToSelect){
+		//browser.focus(browser.div(comboBoxIdentifier));
+		browser.xy(browser.div(comboBoxIdentifier), 3, 3).click();
+		//browser.focus(browser.row(optionToSelect));
+		browser.xy(browser.row(optionToSelect), 3, 3).click();
+		_logger.log(Level.INFO, "Clicked on \""+optionToSelect+"\"");
+	}
+	
+	//Getting array value from String
+	public String[] getCommaToArray(String commaValue){
+		return commaValue.split(",");
+	}
+
+	//String to key value map
+	public HashMap<String, String> getKeyValueMap(String keyValuesString){
+		HashMap<String, String> keyValueMap = new HashMap<String, String>();
+		String[] keyValuesArray = keyValuesString.split(",");
+		for(String keyValue: keyValuesArray){
+			String[] keyVal = keyValue.split("=");
+			if(keyVal.length < 2 ){
+				keyValueMap.put(keyVal[0].trim(), "");
+			}else{
+				keyValueMap.put(keyVal[0].trim(), keyVal[1].trim());
+			}			
+		}
+		return keyValueMap;		
+	}
+	
+	//String to collection of hash map
+	public LinkedList<HashMap<String, String>> getKeyValueMapList(String keyValuesString){
+		LinkedList<HashMap<String, String>> list = new LinkedList<HashMap<String,String>>();
+		HashMap<String, String> keyValueMap = new HashMap<String, String>();
+		String[] keyValuesArray = keyValuesString.split(",");
+		for(String keyValue: keyValuesArray){
+			String[] keyVal = keyValue.split("=");
+			if(keyVal.length < 2 ){
+				keyValueMap.put(keyVal[0].trim(), "");
+			}else{
+				keyValueMap.put(keyVal[0].trim(), keyVal[1].trim());
+			}	
+			list.addLast((HashMap<String, String>) keyValueMap.clone());
+		}
+		return list;		
+	}
+	
+	//Wait until the element get present or timeout, which one is lesser
+	public void waitForElementDivExists(Browser browser, String element, int waitTimeMilliSeconds){
+		waitForElementExists(browser, browser.div(element), element, waitTimeMilliSeconds);
+	}
+
+	public void waitForElementRowExists(Browser browser, String element, int waitTimeMilliSeconds){
+		waitForElementExists(browser, browser.row(element), element, waitTimeMilliSeconds);
+	}
+	
+	public void waitForElementDivVisible(Browser browser, String element, int waitTimeMilliSeconds){
+		waitForElementVisible(browser, browser.row(element), element, waitTimeMilliSeconds);
+	}
+	
+	public void waitForElementRowVisible(Browser browser, String element, int waitTimeMilliSeconds){
+		waitForElementVisible(browser, browser.row(element), element, waitTimeMilliSeconds);
+	}
+
+	public void waitForElementExists(Browser browser, ElementStub elementStub, String element, int waitTimeMilliSeconds){
+		while(waitTimeMilliSeconds >=  0){
+			if(elementStub.exists()){
+				_logger.info("Element \""+elementStub.getText()+"\" is available now!");
+				return;
+			}else{
+				browser.waitFor(500);
+				waitTimeMilliSeconds -= 500;
+				_logger.finer("Waiting for the element: \""+element+"\", Remaining wait time: "+waitTimeMilliSeconds+" milli Second(s)");
+			}
+		}		
+		_logger.warning("Failed to get the element: \""+element+"\" on time!");
+	}
+
+	public void waitForElementVisible(Browser browser, ElementStub elementStub, String element, int waitTimeMilliSeconds){
+		while(waitTimeMilliSeconds >=  0){
+			if(elementStub.isVisible()){
+				_logger.info("Element \""+elementStub.getText()+"\" is visable now!");
+				return;
+			}else{
+				browser.waitFor(500);
+				waitTimeMilliSeconds -= 500;
+				_logger.finer("Waiting for the element: \""+element+"\", Remaining wait time: "+waitTimeMilliSeconds+" milli Second(s)");
+			}
+		}		
+		_logger.warning("Failed to get the element: \""+element+"\" on time!");
 	}
 }
