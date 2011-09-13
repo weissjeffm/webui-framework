7cc9db94bac12b430cc22f031bbad52fcdfa9b9d
Jeff Weiss <jweiss@redhat.com>
2011-09-13 10:23:37 -0400

    Revert "minor change to bz checker hopefully helps multithreading?" it didn't help :) This reverts commit cd43fd8939e4acc523375c9a0fe6e159d357f5f2.

--- src/com/redhat/qe/auto/bz.clj
+++ src/com/redhat/qe/auto/bz.clj
@@ -5,14 +5,14 @@
   (memoize (fn [checker id] (.isBugOpen checker id))))
 
 (defn blocked-by-bz-bugs [ & ids]
-  (let [checker (BzChecker/getInstance)]
-    (with-meta (fn [_]
-                 (let [still-open (filter (fn [id] (docheck checker id))
-                                          ids)]
-                   (if (= 0 (count still-open)) nil
-                       still-open)))
-      {:type :bz-blocker
-       ::source `(~'blocked-by-bz-bugs ~@ids)})))
+  (with-meta (fn [_]
+               (let [checker (BzChecker/getInstance)
+                     still-open (filter (fn [id] (docheck checker id))
+                                        ids)]
+                 (if (= 0 (count still-open)) nil
+                     still-open)))
+    {:type :bz-blocker
+     ::source `(~'blocked-by-bz-bugs ~@ids)}))
 
 (defmethod print-method :bz-blocker [o ^Writer w]
   (print-method (::source (meta o)) w))
